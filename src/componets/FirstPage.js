3baffea2c1ac7f1e20253eefd368082e4d03e9d8
bealun <53275597+bealun@users.noreply.github.com>
2020-05-09 13:31:07 +0200

    styling first page

--- src/componets/FirstPage.js
+++ src/componets/FirstPage.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Link } from "react-router-dom";
-// import { Salty } from './Salty'
+import { Link } from "react-router-dom"
+
+import './style/FirstPage.css'
 
 export const FirstPage = () => {
   return (
-    <div>
-      <h1>Test</h1>
-      <Link to={'/salty'} >
-        <p>Testing again</p>
+    <div className='firstPage'>
+      {/* <h1>Test</h1> */}
+      <Link className='saltyPage' to={'/salty'} >
+        <p>Salty test</p>
       </Link>
 
-      <Link to={'/sweet'} >
+      <Link className='sweetPage' to={'/sweet'} >
         <p>Sweet test</p>
       </Link>
     </div>
