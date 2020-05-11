65f3d91ab825d94faaf6652ac44f984f1c972411
bealun <53275597+bealun@users.noreply.github.com>
2020-05-11 15:39:48 +0200

    base styling for details pages

--- src/componets/ItemSalty.js
+++ src/componets/ItemSalty.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 
+import './style/Details.css'
+
 export const ItemSalty = () => {
   const [salty, setSalty] = useState([])
   const { id } = useParams()
@@ -20,7 +22,15 @@ export const ItemSalty = () => {
         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="#000"><path d="M12 24c6.627 0 12-5.373 12-12s-5.373-12-12-12-12 5.373-12 12 5.373 12 12 12zm-1-17v4h8v2h-8v4l-6-5 6-5z" /></svg>
         <span>Go Back</span>
       </Link>
-      <h1>{salty.name}</h1>
+      <div className="detailWrapper">
+        <img src={salty.img_url} alt={salty.name} />
+        <div className="moreAbout">
+          <h1>{salty.name}</h1>
+          <h3>{salty.typeOf}</h3>
+          <p>Time: {salty.time} minutes</p>
+          <p>{salty.ingredients} ingredients</p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/componets/SweetItem.js
+++ src/componets/SweetItem.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 
+import './style/Details.css'
+
 export const SweetItem = () => {
   const [sweet, setSweet] = useState([])
   const { id } = useParams()
@@ -15,12 +17,20 @@ export const SweetItem = () => {
   }, [id])
 
   return (
-    <div>
+    <div className="contentDetail">
       <Link className='goBack' to='/sweet'>
         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="#000"><path d="M12 24c6.627 0 12-5.373 12-12s-5.373-12-12-12-12 5.373-12 12 5.373 12 12 12zm-1-17v4h8v2h-8v4l-6-5 6-5z" /></svg>
         <span>Go Back</span>
       </Link>
-      <h1>{sweet.name}</h1>
+      <div className="detailWrapper">
+        <img src={sweet.img_url} alt={sweet.name} />
+        <div className="moreAbout">
+          <h1>{sweet.name}</h1>
+          <h3>{sweet.typeOf}</h3>
+          <p>Time: {sweet.time} minutes</p>
+          <p>{sweet.ingredients} ingredients</p>
+        </div>
+      </div>
     </div>
   )
 }
