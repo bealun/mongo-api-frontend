bde879e3541d20598c2d4ead3596f94fcdfa0828
bealun <53275597+bealun@users.noreply.github.com>
2020-05-11 13:44:20 +0200

    fetched list

--- src/componets/Salty.js
+++ src/componets/Salty.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
+import './style/Salty.css'
+
 export const Salty = () => {
+  const [saltItems, setSaltItems] = useState([])
+
+  useEffect(() => {
+    fetch(`https://bealun-mongo-api.herokuapp.com/food`)
+      .then(res => res.json())
+      .then(json => {
+        console.log('Items:', json)
+        setSaltItems(json)
+      })
+  }, [])
+
   return (
     <section>
-      <Link to='/'>
-        Go Back
+      <Link className='goBack' to='/'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="#000"><path d="M12 24c6.627 0 12-5.373 12-12s-5.373-12-12-12-12 5.373-12 12 5.373 12 12 12zm-1-17v4h8v2h-8v4l-6-5 6-5z" /></svg>
+        <span>Go Back</span>
       </Link>
-      <h2>Next test!</h2>
+      
+      <div class="contentWrapper">
+      {saltItems.map(item => (
+        <div className="eachFood" key={item.id}>
+          <img src={item.img_url} alt='foods' />
+          <h4>{item.name}</h4>
+        </div>
+      ))}
+      </div>
     </section>
   )
 }
--- src/componets/Sweet.js
+++ src/componets/Sweet.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 export const Sweet = () => {
+  const [sweetThings, setSweetThings] = useState([])
+
+  useEffect(() => {
+    fetch(`https://bealun-mongo-api.herokuapp.com/dessert`)
+      .then(res => res.json())
+      .then(json => {
+        console.log('Items:', json)
+        setSweetThings(json)
+      })
+  }, [])
+
   return (
-    <div>
-      <Link to='/'>
-        Go Back
+    <section>
+      <Link className='goBack' to='/'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="#000"><path d="M12 24c6.627 0 12-5.373 12-12s-5.373-12-12-12-12 5.373-12 12 5.373 12 12 12zm-1-17v4h8v2h-8v4l-6-5 6-5z" /></svg>
+        <span>Go Back</span>
       </Link>
-      <p>Oh it's me again</p>
-    </div>
+      
+      <div class="contentWrapper">
+      {sweetThings.map(item => (
+        <div className="eachFood" key={item.id}>
+          <img src={item.img_url} alt='foods' />
+          <h4>{item.name}</h4>
+        </div>
+      ))}
+      </div>
+    </section>
   )
 }
